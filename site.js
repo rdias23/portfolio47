4f9a59ae1d957441df5f2afbeaf749a0cb5e9520
rdias23 <rdias23@gmail.com>
2015-01-05 10:25:22 -0500

    updated with asset list slide

--- site.js
+++ site.js
@@ -3,30 +3,30 @@ $(document).ready(function() {
 	//rotation speed and timer
 	var speed = 5000;
 	var run = setInterval('rotate()', speed);
-	
+
 	//grab the width and calculate left value
-	var item_width = $('#slides li:eq(1)').outerWidth(); 
-	var left_value = item_width * (-1); 
-        
+	var item_width = $('#slides li:eq(1)').outerWidth();
+	var left_value = item_width * (-1);
+
     //move the last item before first item
 	$('#slides li:first').before($('#slides li:last'));
-	
-	//set the default item to the correct position 
+
+	//set the default item to the correct position
 	$('#slides ul').css({'left' : left_value});
 
     //if user clicked on prev button
 	$('#prev').click(function() {
 
-		//get the right position            
+		//get the right position
 		var left_indent = parseInt($('#slides ul').css('left')) + item_width;
 
-		//slide the item            
-		$('#slides ul').animate({'left' : left_indent}, 200,function(){    
+		//slide the item
+		$('#slides ul').animate({'left' : left_indent}, 200,function(){
 
-            //move the last item and put it as first item            	
+            //move the last item and put it as first item
 			$('#slides li:first').before($('#slides li:last'));
-		
-			if($('#slides li:eq(1)').attr("id") == "trivia_link")
+
+			                  if($('#slides li:eq(1)').attr("id") == "trivia_link")
                         {
                                 $("#trivia_info").toggle("slide");
                                 $("#portfolio47_info").toggle("slide");
@@ -34,31 +34,36 @@ $(document).ready(function() {
                         if($('#slides li:eq(1)').attr("id") == "wishlist_link")
                         {
                                 $("#wishlist_info").toggle("slide");
-			        $("#wizards_and_spaceships_info").toggle("slide");
-   			}
+			                          $("#wizards_and_spaceships_info").toggle("slide");
+   			                }
                         if($('#slides li:eq(1)').attr("id") == "art_link")
                         {
-				$("#art_info").toggle("slide");
-				$("#trivia_info").toggle("slide");
+                        				$("#art_info").toggle("slide");
+                        				$("#trivia_info").toggle("slide");
                         }
-			if($('#slides li:eq(1)').attr("id") == "portfolio47_link")
+			                  if($('#slides li:eq(1)').attr("id") == "portfolio47_link")
                         {
                                 $("#portfolio47_info").toggle("slide");
                                 $("#wishlist_info").toggle("slide");
                         }
-			if($('#slides li:eq(1)').attr("id") == "wizards_and_spaceships_link")
+			                  if($('#slides li:eq(1)').attr("id") == "wizards_and_spaceships_link")
                         {
-				$("#legislator_info").toggle("slide");	
-				$("#wizards_and_spaceships_info").toggle("slide"); 
-			}
-			if($('#slides li:eq(1)').attr("id") == "legislator_lookup_link")
+                        				$("#legislator_info").toggle("slide");
+                        				$("#wizards_and_spaceships_info").toggle("slide");
+			                   }
+			                   if($('#slides li:eq(1)').attr("id") == "legislator_lookup_link")
                         {
-				$("#legislator_info").toggle("slide");
-                       		$("#film_info").toggle("slide"); 
-			}
-			if($('#slides li:eq(1)').attr("id") == "film_fan_link")
+				                        $("#legislator_info").toggle("slide");
+                       		      $("#film_info").toggle("slide");
+			                   }
+			                   if($('#slides li:eq(1)').attr("id") == "film_fan_link")
                         {
                                 $("#film_info").toggle("slide");
+                                $("#asset_info").toggle("slide");
+                        }
+                         if($('#slides li:eq(1)').attr("id") == "asset_link")
+                        {
+                                $("#asset_info").toggle("slide");
                                 $("#art_info").toggle("slide");
                         }
 
@@ -74,62 +79,68 @@ $(document).ready(function() {
                         $('#slides li:eq(2)').height(250).width(250);
                         $('#slides li:eq(2) img').height(250).width(250);
                         $('#slides li:eq(0)').height(250).width(250);
-                        $('#slides li:eq(0) img').height(250).width(250);		
+                        $('#slides li:eq(0) img').height(250).width(250);
 		});
 
-		//cancel the link behavior            
+		//cancel the link behavior
 		return false;
-            
+
 	});
 
- 
+
     //if user clicked on next button
 	$('#next').click(function() {
-		
+
 		//get the right position
 		var left_indent = parseInt($('#slides ul').css('left')) - item_width;
-		
+
 		//slide the item
 		$('#slides ul').animate({'left' : left_indent}, 200, function () {
-            
+
             //move the first item and put it as last item
-			$('#slides li:last').after($('#slides li:first'));                 	
+			$('#slides li:last').after($('#slides li:first'));
 
 
-			if($('#slides li:eq(1)').attr("id") == "trivia_link")
+			                  if($('#slides li:eq(1)').attr("id") == "trivia_link")
                         {
                                 $("#trivia_info").toggle("slide");
-				$("#art_info").toggle("slide");
+				                        $("#art_info").toggle("slide");
                         }
                         if($('#slides li:eq(1)').attr("id") == "wishlist_link")
-			{
-				$("#wishlist_info").toggle("slide");
-				$("#portfolio47_info").toggle("slide");
-			}
-			if($('#slides li:eq(1)').attr("id") == "art_link")
-			{
-				$("#art_info").toggle("slide");
-				$("#film_info").toggle("slide");
+                  			{
+                        				$("#wishlist_info").toggle("slide");
+                        				$("#portfolio47_info").toggle("slide");
+                  			}
+			                  if($('#slides li:eq(1)').attr("id") == "art_link")
+                  			{
+                        				$("#art_info").toggle("slide");
+                                $("#asset_info").toggle("slide");
+                        				// $("#film_info").toggle("slide");
                         }
-			if($('#slides li:eq(1)').attr("id") == "portfolio47_link")
-			{
-				$("#portfolio47_info").toggle("slide");
-				$("#trivia_info").toggle("slide");
-			}
-			if($('#slides li:eq(1)').attr("id") == "wizards_and_spaceships_link")
+                  			if($('#slides li:eq(1)').attr("id") == "portfolio47_link")
+                  			{
+                        				$("#portfolio47_info").toggle("slide");
+                        				$("#trivia_info").toggle("slide");
+                  			}
+			                  if($('#slides li:eq(1)').attr("id") == "wizards_and_spaceships_link")
                         {
-				$("#wizards_and_spaceships_info").toggle("slide"); 
-				$("#wishlist_info").toggle("slide");
-			}	
-			if($('#slides li:eq(1)').attr("id") == "legislator_lookup_link")
+                        				$("#wizards_and_spaceships_info").toggle("slide");
+                        				$("#wishlist_info").toggle("slide");
+                  			}
+			                  if($('#slides li:eq(1)').attr("id") == "legislator_lookup_link")
                         {
                                 $("#legislator_info").toggle("slide");
                                 $("#wizards_and_spaceships_info").toggle("slide");
                         }
-			if($('#slides li:eq(1)').attr("id") == "film_fan_link")
+                  			if($('#slides li:eq(1)').attr("id") == "film_fan_link")
                         {
-				$("#film_info").toggle("slide");
+				                        $("#film_info").toggle("slide");
                                 $("#legislator_info").toggle("slide");
+                        }
+                         if($('#slides li:eq(1)').attr("id") == "asset_link")
+                        {
+                                $("#asset_info").toggle("slide");
+                                $("#film_info").toggle("slide");
                         }
 
 			//set the default item to correct position
@@ -137,36 +148,37 @@ $(document).ready(function() {
 			$('#slides li:eq(3)').hide();
 			$('#slides li:eq(4)').hide();
 			$('#slides li:eq(5)').hide();
-			$('#slides li:eq(6)').hide();	
+			$('#slides li:eq(6)').hide();
+      $('#slides li:eq(7)').hide();
 			$('#slides li:eq(1)').height(300).width(300);
 			$('#slides li:eq(1) img').height(300).width(300);
 			$('#slides li:eq(2)').show();
 			$('#slides li:eq(2)').height(250).width(250);
 			$('#slides li:eq(2) img').height(250).width(250);
 			$('#slides li:eq(0)').height(250).width(250);
-			$('#slides li:eq(0) img').height(250).width(250);	
+			$('#slides li:eq(0) img').height(250).width(250);
 		});
 
 		//cancel the link behavior
 		return false;
-		
-	});        
-	
+
+	});
+
 	//if mouse hover, pause the auto rotation, otherwise rotate it
 	$('#slides, #buttons').hover(
-		
+
 		function() {
 			clearInterval(run);
-		}, 
+		},
 		function() {
-			run = setInterval('rotate()', speed);	
+			run = setInterval('rotate()', speed);
 		}
-	); 
-        
+	);
+
 });
 
 //a simple function to click next link
-//a timer will call this function, and the rotation will begin :)  
+//a timer will call this function, and the rotation will begin :)
 function rotate() {
 	$('#prev').click();
 }
